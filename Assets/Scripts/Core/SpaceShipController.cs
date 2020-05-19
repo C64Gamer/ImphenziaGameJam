3abf1f93fe93d6f67917060a8ec5772bf5b1117c
C64Gamer <info@c64gamer.com>
2020-05-19 04:27:10 +0200

    Adding SpaceShipController

---
+++ Assets/Scripts/Core/SpaceShipController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SpaceShipController : MonoBehaviour
+{
+
+    [Header("General Settings")] 
+    public bool isActive = true;
+    public float shipSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
