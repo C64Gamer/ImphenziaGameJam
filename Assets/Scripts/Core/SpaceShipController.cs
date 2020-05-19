0502fb3c6828a04833330534d25436d3fd46ae9f
C64Gamer <info@c64gamer.com>
2020-05-19 10:01:21 +0200

    Adding basic spaceship movement

--- Assets/Scripts/Core/SpaceShipController.cs
+++ Assets/Scripts/Core/SpaceShipController.cs
@@ -1,21 +1,67 @@
 ﻿using UnityEngine;
 
-public class SpaceShipController : MonoBehaviour
+namespace Core
 {
-
-    [Header("General Settings")] 
-    public bool isActive = true;
-    public float shipSpeed = 1f;
-
-    // Start is called before the first frame update
-    void Start()
+    public class SpaceShipController : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("General Settings")] public bool isActive = true;
+        public float shipSpeed = 1;
+        public float sideForce = 130;
+        public float maxAngle = 35;
+        public float resetSpeed = 130;
+
+        // Update is called once per frame
+        void Update()
+        {
+            CheckMaxFlightAngle();
+            DoBasicMovement();
+        }
+
+        private void CheckMaxFlightAngle()
+        {
+            var currentZRotation = UnityEditor.TransformUtils.GetInspectorRotation(transform).z;
+            
+            //TODO Cleanup code
+            if (currentZRotation < -maxAngle)
+            {
+                Vector3 tmp = transform.rotation.eulerAngles;
+                tmp.z = -maxAngle;
+                Debug.Log(tmp);
+                transform.rotation = Quaternion.Euler(tmp);
+            }
+
+            if (currentZRotation > maxAngle)
+            {
+                Vector3 tmp = transform.rotation.eulerAngles;
+                tmp.z = maxAngle;
+                transform.rotation = Quaternion.Euler(tmp);
+            }
+        }
+
+        private void DoBasicMovement()
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += Vector3.left * (shipSpeed * Time.deltaTime);
+                transform.Rotate(0, 0, sideForce * Time.deltaTime, Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += Vector3.right * (shipSpeed * Time.deltaTime);
+                transform.Rotate(0, 0, -sideForce * Time.deltaTime, Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += Vector3.forward * (shipSpeed * Time.deltaTime);
+            }
+            
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += Vector3.back * (shipSpeed * Time.deltaTime);
+            }
+        }
     }
 }
